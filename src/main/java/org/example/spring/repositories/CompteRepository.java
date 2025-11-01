2a244e5be13c492af276188af58a3431da179dc6
Elidrissi12 <zakiabdo122@gmail.com>
2025-11-01 09:38:19 +0100

    new

---
+++ src/main/java/org/example/spring/repositories/CompteRepository.java
@@ -0,0 +1,9 @@
+package org.example.spring.repositories;
+
+import org.example.spring.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+}
